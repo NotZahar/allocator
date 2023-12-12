0c97682eff58ccb7f8f9f4ecf671d7e2f6e6d4be
Nazar Glukhov <nazar.glukhov@gmail.com>
2023-12-12 07:18:51 +0300

    remove all files for review

--- libs/Allocator/include/helper.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-struct messages {
-    static inline const std::string NOT_ENOUGH_MEMORY = "Not enough memory";
-    static inline const std::string BAD_ARGUMENT = "Bad argument";
-    static inline const std::string ALREADY_FREE = "Memory is already free";
-};
